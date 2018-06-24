ee03cf11b6df3af0d33ea914c54ee5816fca16e7
praful <ankitmali@yandex.com>
2018-06-24 12:05:37 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# tutorial LINK (script) #
+[voting-system-php-tutorial](http://webeasystep.com/blog/view_article/How_to_build_voting_Ajax_system_with_Codeigniter_part_2)
+
+# video LINK #
+[voting-system-codeigniter-video](https://www.youtube.com/watch?v=oms1ZcWBDY4)
---
+++ application/logs/log-2018-06-24.php
@@ -0,0 +1,82 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+DEBUG - 2018-06-24 08:09:32 --> No URI present. Default controller set.
+ERROR - 2018-06-24 08:09:32 --> Severity: Warning --> mysqli::real_connect(): (HY000/1049): Unknown database 'demo' C:\xampp\htdocs\codeigniter-voting\system\database\drivers\mysqli\mysqli_driver.php 202
+ERROR - 2018-06-24 08:09:32 --> Severity: error --> Exception: Unable to connect to the database. C:\xampp\htdocs\codeigniter-voting\system\database\DB_driver.php 433
+DEBUG - 2018-06-24 08:10:08 --> No URI present. Default controller set.
+ERROR - 2018-06-24 08:10:08 --> Query error: Table 'vadodara.ci_sessions' doesn't exist - Invalid query: SELECT `data`
+FROM `ci_sessions`
+WHERE `id` = '1pn4kdpg647v3thv1qk9p3k4ga'
+DEBUG - 2018-06-24 08:10:28 --> No URI present. Default controller set.
+ERROR - 2018-06-24 08:10:28 --> Query error: Table 'ci-social-login.ci_voting' doesn't exist - Invalid query: SELECT *
+FROM `ci_voting`
+WHERE `dv_state` = 1
+DEBUG - 2018-06-24 08:12:24 --> No URI present. Default controller set.
+ERROR - 2018-06-24 08:12:57 --> Could not find the language line "dv_title"
+ERROR - 2018-06-24 08:13:05 --> Could not find the language line "dv_title"
+ERROR - 2018-06-24 08:13:05 --> Could not find the language line "act"
+ERROR - 2018-06-24 08:13:10 --> Could not find the language line "dv_title"
+ERROR - 2018-06-24 08:13:25 --> Could not find the language line "dv_title"
+ERROR - 2018-06-24 08:13:25 --> Could not find the language line "act"
+ERROR - 2018-06-24 08:13:25 --> Could not find the language line "act"
+ERROR - 2018-06-24 08:13:39 --> Could not find the language line "act"
+ERROR - 2018-06-24 08:13:39 --> Could not find the language line "act"
+ERROR - 2018-06-24 08:13:41 --> Could not find the language line "inact"
+ERROR - 2018-06-24 08:13:41 --> Could not find the language line "act"
+ERROR - 2018-06-24 08:13:42 --> Could not find the language line "act"
+ERROR - 2018-06-24 08:13:42 --> Could not find the language line "inact"
+ERROR - 2018-06-24 08:13:55 --> Could not find the language line "act"
+ERROR - 2018-06-24 08:13:55 --> Could not find the language line "inact"
+ERROR - 2018-06-24 08:13:57 --> Could not find the language line "act"
+ERROR - 2018-06-24 08:13:57 --> Could not find the language line "inact"
+ERROR - 2018-06-24 08:13:59 --> Could not find the language line "act"
+ERROR - 2018-06-24 08:13:59 --> Could not find the language line "inact"
+ERROR - 2018-06-24 08:16:47 --> Could not find the language line "inact"
+ERROR - 2018-06-24 08:16:47 --> Could not find the language line "inact"
+ERROR - 2018-06-24 08:17:01 --> Could not find the language line "inact"
+ERROR - 2018-06-24 08:17:01 --> Could not find the language line "inact"
+ERROR - 2018-06-24 08:17:04 --> Could not find the language line "dv_title"
+ERROR - 2018-06-24 08:17:05 --> Could not find the language line "inact"
+ERROR - 2018-06-24 08:17:05 --> Could not find the language line "inact"
+DEBUG - 2018-06-24 08:20:58 --> No URI present. Default controller set.
+ERROR - 2018-06-24 08:21:04 --> Could not find the language line "inact"
+ERROR - 2018-06-24 08:21:04 --> Could not find the language line "inact"
+ERROR - 2018-06-24 08:21:28 --> Could not find the language line "inact"
+ERROR - 2018-06-24 08:21:28 --> Could not find the language line "inact"
+ERROR - 2018-06-24 08:21:33 --> Could not find the language line "inact"
+ERROR - 2018-06-24 08:21:33 --> Could not find the language line "inact"
+ERROR - 2018-06-24 08:21:58 --> Could not find the language line "inact"
+ERROR - 2018-06-24 08:21:58 --> Could not find the language line "inact"
+ERROR - 2018-06-24 08:22:06 --> Could not find the language line "inact"
+ERROR - 2018-06-24 08:22:08 --> Could not find the language line "inact"
+ERROR - 2018-06-24 08:22:11 --> Could not find the language line "inact"
+ERROR - 2018-06-24 08:22:12 --> Could not find the language line "inact"
+ERROR - 2018-06-24 08:22:14 --> Could not find the language line "inact"
+ERROR - 2018-06-24 08:22:18 --> Could not find the language line "inact"
+ERROR - 2018-06-24 08:22:27 --> Could not find the language line "inact"
+ERROR - 2018-06-24 08:23:18 --> Could not find the language line "inact"
+ERROR - 2018-06-24 08:23:20 --> Could not find the language line "inact"
+ERROR - 2018-06-24 08:23:22 --> Could not find the language line "inact"
+ERROR - 2018-06-24 08:23:31 --> Could not find the language line "inact"
+ERROR - 2018-06-24 08:23:33 --> Could not find the language line "inact"
+ERROR - 2018-06-24 08:23:37 --> Could not find the language line "inact"
+ERROR - 2018-06-24 08:24:03 --> Could not find the language line "inact"
+ERROR - 2018-06-24 08:24:35 --> Could not find the language line "inact"
+ERROR - 2018-06-24 08:24:37 --> Could not find the language line "dv_title"
+ERROR - 2018-06-24 08:24:45 --> Could not find the language line "dv_title"
+ERROR - 2018-06-24 08:24:45 --> Could not find the language line "inact"
+ERROR - 2018-06-24 08:24:45 --> Could not find the language line "act"
+ERROR - 2018-06-24 08:24:48 --> Could not find the language line "dv_title"
+ERROR - 2018-06-24 08:24:54 --> Could not find the language line "dv_title"
+ERROR - 2018-06-24 08:24:54 --> Could not find the language line "inact"
+ERROR - 2018-06-24 08:24:54 --> Could not find the language line "act"
+ERROR - 2018-06-24 08:24:56 --> Could not find the language line "dv_title"
+ERROR - 2018-06-24 08:24:59 --> Could not find the language line "dv_title"
+ERROR - 2018-06-24 08:24:59 --> Could not find the language line "inact"
+ERROR - 2018-06-24 08:24:59 --> Could not find the language line "act"
+ERROR - 2018-06-24 08:25:06 --> Could not find the language line "dv_title"
+ERROR - 2018-06-24 08:25:12 --> Could not find the language line "dv_title"
+ERROR - 2018-06-24 08:25:12 --> Could not find the language line "dv_title"
+ERROR - 2018-06-24 08:25:12 --> Could not find the language line "inact"
+ERROR - 2018-06-24 08:25:12 --> Could not find the language line "inact"
+ERROR - 2018-06-24 08:25:12 --> Could not find the language line "inact"
